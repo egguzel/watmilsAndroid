3f04e6b6044b3d484223faf5d11bb1899acc7785
egguzel <egguzel@gmail.com>
2018-01-23 16:51:24 +0300

    Proje İlklendirmesi

---
+++ app/src/main/java/com/lailial/watmils/MainActivity.java
@@ -0,0 +1,56 @@
+package com.lailial.watmils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after defined time
+
+                if(isNetworkStatusAvialable (getApplicationContext())) {
+                    startActivity(new Intent(MainActivity.this,WebViewActivity.class));
+                } else {
+                    Toast.makeText(getApplicationContext(), "internet is not available", Toast.LENGTH_SHORT).show();
+
+                }
+
+                finish();
+
+            }
+        }, 3000);
+
+
+    }
+
+    public static boolean isNetworkStatusAvialable (Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null)
+        {
+            NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
+            if(netInfos != null)
+                if(netInfos.isConnected())
+                    return true;
+        }
+        return false;
+    }
+}
+
